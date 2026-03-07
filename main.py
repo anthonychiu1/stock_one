f72a59afd51cfb88e378b65c2e8afc88b0daeeee
anthonychiumb <chiua1688@gmail.com>
2026-03-06 16:30:21 -0800

    added template to yml file, file now creates json data from av using stock ticker, added generated stock file to gitingore

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+import smtplib
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+NUMBER_OF_DAYS = 2
+
+load_dotenv()
+alpha_api_key = os.getenv("ALPHAVANTAGE_API_KEY")
+
+def get_alphavantage_data():
+    av_params = {
+        "function":"TIME_SERIES_DAILY",
+        "symbol": STOCK,
+        "apikey":alpha_api_key
+                 }
+    alphavantage_url = "https://www.alphavantage.co/query"
+    response = requests.get(alphavantage_url,params=av_params)
+    data = response.json()
+    data["Time Series (Daily)"] = dict(list(data["Time Series (Daily)"].items())[:NUMBER_OF_DAYS])
+
+    with open("stock_data_file.json", mode="w") as file:
+        json.dump(data,file,indent=2)
+
+def send_email():
+    pass
+    
+get_alphavantage_data()
+
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
